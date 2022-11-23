72b25a9c2bd2d923bcd17287dd5ae333b1abe52d
[Shann Roberts] <[dpr.1979@bigpond.com]>
2022-11-23 09:24:28 +0800

    create unit test case file + annotate

--- calculate.js
+++ calculate.js
@@ -1,3 +1,5 @@
+//this js file will contain the working code snippet 
+//this is a variable containing multiple functions
 const calculate = {
     sum: function(a,b) {
         return a + b;
@@ -9,5 +11,10 @@ const calculate = {
         return a * b;
     }
 }
-
+//using common.js module.export to 'move' variable data
 module.exports = calculate;
+
+//ADOPTING TEST AS YOU CODE APPROACH:
+//therefore, next step is to create a corresponding 
+//unit test case for this file (eg .spec.js) 
+//this assignment has asked for .test.js 
---
+++ calculate.test.js
@@ -0,0 +1,7 @@
+//this file will contain the unit tests for the corresponding
+// file calculate.js which contains the code I need to test
+
+//in order to test the code I need to import it
+
+const mathOperations = require('./calculate')
+
