9abd3d4eeb039a09ee8c9d02aea0364d90de79e8
[Shann Roberts] <[dpr.1979@bigpond.com]>
2022-11-24 16:59:40 +0800

    const fixed, sum + sum unit test working

--- calculate.js
+++ calculate.js
@@ -1,18 +1,18 @@
 //this js file will contain the working code snippet 
 //this is a variable containing multiple functions
-const calculate = {
-    sum: function(a,b) {
+const calculate = 
+    {sum: function(a,b) {
         return a + b;
-    },
-    subtract: function(a,b) {
-        return a - b;
-    },
-    multiply: function(a,b) {
-        return a * b;
-    }
-}
+    }}
+//     ,subtract: function(a,b) {
+//         return a - b;
+//     },
+//     multiply: function(a,b) {
+//         return a * b;
+//     }
+// }
 //using common.js module.export to 'move' variable data
-module.exports = calculate;
+module.exports = { calculate }
 
 //ADOPTING TEST AS YOU CODE APPROACH:
 //therefore, next step is to create a corresponding 
--- calculate.test.js
+++ calculate.test.js
@@ -2,8 +2,36 @@
 // file calculate.js which contains the code I need to test
 
 //in order to test the code I need to import it
+//I have changed the const name to calcualte to correspond
+//with the const name supplied in instructions of assignment. 
+//const name in step 2 does not correlate so not used
 
-const mathOperations = require('./calculate');
+const { calculate } = require('./calculate');
 
+//below used to confirm require working 
+// console.log(calculate);
+
+//write a test for adding 2 numbers and validate the expected result
+
+//declare a & b
+
+const a = 6;
+const b = 5;
+
+
+describe ("Unit tests for checking summation function", () => 
+        it("should return 11", () => {
+        sumIn = calculate.sum(a,b);
+        expect(sumIn).toBe(11);
+}));
+
+//npm run test successful after several failures
+
+
+  
+//     it("should return false", () => {
+//        finalIn = calculate.sum(a,'b');
+//        expect(finalIn).toBe(false);
+// });
 
 
